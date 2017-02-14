1ad708a21692723bc506c83515f44b89159492d2
Azhary <prof.alazhary@gmail.com>
2017-02-14 22:24:32 +0200

    ay 7agaaa

---
+++ DB Classes/connection.php
@@ -0,0 +1,16 @@
+<?php
+require 'config.php';
+class connection
+{
+    static function conn()
+    {
+      $conn = new mysqli(DBHOST, DBUSER, DBPASS, DBNAME);
+      if($conn->connect_errno)
+      {
+        echo("connection to DB faild<br>".$conn->connect_error);
+        return false;
+      }
+      return $conn;
+    }
+  }
+ ?>
