210161f45d84c33073c9428f4647757da0c1ef9d
Al-shimaa Adel <alshimaa.adel11@gmail.com>
2017-02-18 09:17:57 +0200

    updated product class

--- DBClasses/ProductClass.php
+++ DBClasses/ProductClass.php
@@ -5,30 +5,27 @@ class ProductClass{
 	private $product_id;
 	private $cat_id;
 	private $product_name;
-	private $img_path ;
+	private $img_path;
 	private $description;
 	private $price;
 	private $quantity;
-
+	
 	function __construct(){
 
 	}
-
-
 
 	// //Start of Constructor
 	// function __construct($product_id,$cat_id,$product_name,$img_path,$description,$price
 	// 	){
 	// 	$this->product_id =isset($this->product_id )?$this->product_id:$product_id ;
 	// 	$this->cat_id=isset($this->cat_id)?$this->cat_id:$cat_id;
-	// 	$this->product_name=isset($this->product_name)?$this->product_name:$product_name;
+	// 	$this->product_name=isset($this->product_name)?$this->product_name:$product_name;	
 	// 	$this->img_path=isset($this->img_path)?$this->img_path:$img_path;
 	// 	$this->description=isset($this->description)?$this->description:$description;
 	// 	$this->price=isset($this->price)?$this->price:$price;
 	// 	//$this->quantity=isset($this->quantity)?$this->quantity:$quantity;
 	// 	//$this->updated_at=isset($this->updated_at)?$this->updated_at:$updated_at;
 	//  }  //End of Constructor
-
 
 	//Setter Function
 	function __set($attr , $val){
@@ -49,7 +46,7 @@ class ProductClass{
 		if($conn->connect_errno){
 			echo "error connection to DB ".$conn->connect_error."<br>";
 			$success = false;
-		} //End of open connection
+		} //End of open connection  
 
 		$query = "insert into product (product_id,cat_id,product_name,img_path,description,price,quantity) values(?,?,?,?,?,?,?)";
 		$statement = $conn->prepare($query);
@@ -83,7 +80,7 @@ class ProductClass{
 			echo "error connection to DB ".$conn->connect_error."<br>";
 			$success = false;
 		} //End of open connection
-
+		
 
 		$query = "select * from product where product_id=?";
 		$statement = $conn->prepare($query);
@@ -117,7 +114,7 @@ class ProductClass{
 			echo "error connection to DB ".$conn->connect_error."<br>";
 			$success = false;
 		} //End of open connection
-
+		
 
 		$query = "select * from product where cat_id=?";
 		$statement = $conn->prepare($query);
@@ -125,7 +122,7 @@ class ProductClass{
 			echo "error preparing query : ".$conn->error."<br>";
 			$success = false;
 		}
-
+		
 		$result = $statement->bind_param("i",$cat_id);
 		if(!$result){
 			echo "binding failed : ".$statement->error;
@@ -141,7 +138,7 @@ class ProductClass{
 		$products = [];
 		$params = array('product_id','cat_id','product_name','img_path','description','price','quantity');
 		while($product = $result->fetch_object('ProductClass',$params)){
-			$products[] = $product;
+			$products[] = $product; 
 		}
 
 		$statement->close();
@@ -243,13 +240,46 @@ class ProductClass{
 		$products = [];
 		$params = array('product_id','cat_id','product_name','img_path','description','price','quantity');
 		while($product = $result->fetch_object('ProductClass',$params)){
-			$products[] = $product;
+			$products[] = $product; 
 		}
 
 		$statement->close();
 		$conn->close();
 		return $products;
 	}//End of allProducts function
+
+	static function getUserInterests($user_id){
+		$success = true;
+		$conn = new mysqli(DBHOST , DBUSER , DBPASS, DBNAME);
+
+		if($conn->connect_errno){
+			echo "error connection to DB ".$conn->connect_error."<br>";
+			$success = false;
+		} //End of open connection
+
+		$query = "select * from product where cat_id = (select cat_id from interest where user_id=2)";
+		$statement = $conn->prepare($query);
+		if(!$statement){
+			echo "error preparing query : ".$conn->error."<br>";
+			$success = false;
+		}
+
+		if(!$statement->execute()){
+			echo "execution failed : ".$statement->error;
+			$success=false;
+		}// End of insert method
+
+		$result = $statement->get_result();
+		$products = [];
+		$params = array('product_id','cat_id','product_name','img_path','price');
+		while($product = $result->fetch_object('ProductClass',$params)){
+			$products[] = $product;
+		}
+
+		$statement->close();
+		$conn->close();
+		return $products;
+	}//End of intersets products function
 
 	function delete(){
 		$success = true;
@@ -316,14 +346,26 @@ class ProductClass{
 
 }// End of ProductClass Class
 
-//'product_id','cat_id','product_name','img_path','description','price'
-// $product = new ProductClass(170,2,"verna",'ddd','dsds',3000);
+//insert into product (product_id,cat_id,product_name,img_path,description,price,quantity
+// $product = new ProductClass();
+// $product->product_id='150';
+// $product->cat_id='3';
+// $product->product_name='gigi';
+// $product->img_path='gigi';
+// $product->description='jkkjhk';
+// $product->price='150';
+// $product->quantity='150';
 // if($product->insert()){
 // 	echo $product->name."Inserted Succssefully";
 //  }
 
 // $product = ProductClass::getById(1);
 // var_dump($product);
+
+// $product = ProductClass::getUserInterests(1);
+// echo"<pre>";
+// var_dump($product);
+// echo"</pre>";
 
 // $product = ProductClass::getByCatId(1);
 // echo "<pre>";
